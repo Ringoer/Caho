f98abad4b09eb099d75eeff5f863ab2c143a6842
Ringoer <ringoer@qq.com>
2021-09-09 19:24:56 +0800

    修复了版块头像大小错误的问题，并重制了分页器逻辑

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,6 +8,8 @@ interface PaginationProps {
   perPage?: number;
 }
 
+const fill = [-2, -1, 0, 1, 2];
+
 const Pagination = (props: PaginationProps) => {
   const {
     selectedPage: defaultPage = 1,
@@ -18,34 +20,42 @@ const Pagination = (props: PaginationProps) => {
 
   const selectedPage = useMemo(() => +defaultPage, [defaultPage]);
 
-  if (!(+selectedPage && selectedPage % 1 === 0 && selectedPage > 0)) {
+  if (!(selectedPage && selectedPage % 1 === 0 && selectedPage > 0)) {
     return <div>页码错误</div>;
   }
   const [pages, setPages] = useState<number[]>([]);
   const [maxPage, setMaxPage] = useState(0);
+
   useEffect(() => {
     const maxPage =
       Number.isInteger(+count) && +count > 0
-        ? parseInt(((+count + perPage - 1) / perPage).toString())
+        ? Math.floor((+count + perPage - 1) / perPage)
         : 0;
     setMaxPage(maxPage);
-    const start: number = selectedPage > 2 ? -2 : selectedPage > 1 ? -1 : 0;
-    const end = maxPage || -1;
-    const currentMaxPage = 4 + start + +selectedPage;
-    const beta = currentMaxPage - end < 0 ? 0 : currentMaxPage - end;
-    const tmp: number[] =
-      end < 6 && end !== -1
-        ? [1, 2, 3, 4, 5].slice(0, end)
-        : end === -1 || beta === 0
-        ? [0, 1, 2, 3, 4].map((item: number) => item + start + +selectedPage)
-        : [0, 1, 2, 3, 4]
-            .map((item: number) => item + start + +selectedPage)
-            .map((item: number) => item - beta);
+    const indexes = fill
+      .map((item) => {
+        const ans = selectedPage + item;
+        return ans < 1 ? ans + 5 : ans;
+      })
+      .sort((a, b) => a - b);
+    const tmp = indexes
+      .reduce((array, index) => {
+        if (index > maxPage) {
+          if (index > 5) {
+            return [...array, index - 5];
+          } else {
+            return array;
+          }
+        }
+        return [...array, index];
+      }, [] as number[])
+      .sort((a, b) => a - b);
     setPages(tmp);
   }, [selectedPage, count]);
+
   const [target, setTarget] = useState('');
   const turnTo = (page: string | number) => {
-    let target = parseInt(page.toString());
+    const target = parseInt(page.toString());
     if (
       !(
         typeof target === 'number' &&
@@ -61,15 +71,15 @@ const Pagination = (props: PaginationProps) => {
   return (
     <ul className={styles.pagination}>
       <li>
-        <a onClick={() => turnTo(+selectedPage - 1)}>&lt;</a>
+        <a onClick={() => turnTo(selectedPage - 1)}>&lt;</a>
       </li>
       {pages.map((item: number) => (
-        <li key={item} className={item === +selectedPage ? styles.active : ''}>
+        <li key={item} className={item === selectedPage ? styles.active : ''}>
           <a onClick={() => turnTo(item)}>{item}</a>
         </li>
       ))}
       <li>
-        <a onClick={() => turnTo(+selectedPage + 1)}>&gt;</a>
+        <a onClick={() => turnTo(selectedPage + 1)}>&gt;</a>
       </li>
       <li className={styles.turnTo}>
         跳转到
